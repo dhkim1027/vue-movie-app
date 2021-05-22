8918d144d4ff2689b9242bb1ad602fe339c23ead
dhkim1027 <dhkim1027@gmail.com>
2021-05-22 10:46:02 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'DHKIM',
+    email: 'dhkim@abc.com',
+    blog: 'dhkim.blog',
+    phone: '+1-1234-4567',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
